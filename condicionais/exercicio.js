e435fcd6e2989f6b823f7bc7c0d78e601cd64759
veronica2024 <vn710949@gmail.com>
2024-04-11 10:44:20 -0300

    modificado

---
+++ condicionais/exercicio.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro e ${vel}`)
+if(vel>60){
+console.log('dirija sempre usando o cinto de segurança')}
+
+console.log('voce ultrapassou a velocidade permitida, MULTADO.')
---
+++ condicionais/exercicio2.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`vivendo no ${pais}`)
+if(pais == 'Brasil')
+console.log('voçê é Brasileiro')
+else{
+    console.log ('voçê é estrangeiro')
+}
